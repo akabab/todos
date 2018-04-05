5910060a6f3550770b92027636288b1b51586bfa
Yoann Cribier <yoann_cribier@hotmail.com>
2018-04-05 12:26:36 +0200

    todo.js script added for todo detailed view, fetch todo with id (query string)

--- client/js/components/todo.js
+++ client/js/components/todo.js
@@ -1,7 +1,14 @@
 export const createTodoElement = todo => `
+  <a class='todo'>
+    <span>☆ ${todo.stars.length}</span>
+    <h4>${todo.title}</h4>
+  </a>
+`
+
+export const createDetailedTodoElement = todo => `
   <div class='todo'>
+    <span>☆ ${todo.stars.length}</span>
     <h4>${todo.title}</h4>
     <p>${todo.description}</p>
-    <div>☆ ${todo.stars.length}</div>
   </div>
 `
