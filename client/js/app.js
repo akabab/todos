04f80988cb954c127f832432ad2d095a5f9eeac5
Yoann Cribier <yoann_cribier@hotmail.com>
2018-04-09 13:52:20 +0200

    prepare

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,9 @@ import { createTodoElement } from './components/todo.js'
 fetch('http://localhost:3247/todos')
   .then(response => response.json())
   .then(todos => {
-    const todosElement = document.getElementById('todos')
+    const todosContainer = document.getElementById('todos')
 
-    todosElement.innerHTML = todos.map(createTodoElement).join('')
+    const todosElements = todos.map(createTodoElement).join('')
+
+    todosContainer.innerHTML = todosElements
   })
