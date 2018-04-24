2d128f18f74ca4e3eccfb454c8e5d65e09c2ab72
Clement Denis <le.mikmac@gmail.com>
2018-04-24 20:03:20 +0200

    add todo form

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,27 @@
 import { createTodoElement } from './components/todo.js'
 
+const render = todos => {
+  const todosElement = document.getElementById('todos')
+
+  const todoRow = todo => `
+    <div class="row">
+      <div class="column">${createTodoElement(todo)}</div>
+    </div>`
+
+  todosElement.innerHTML = todos.map(todoRow).join('')
+}
+
 fetch('http://localhost:3247/todos')
   .then(response => response.json())
-  .then(todos => {
-    const todosElement = document.getElementById('todos')
+  .then(render)
 
-    const todoRow = todo => `
-      <div class="row">
-        <div class="column">${createTodoElement(todo)}</div>
-      </div>`
-
-    todosElement.innerHTML = todos.map(todoRow).join('')
+const form = document.getElementById('add-todo-form')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch('http://localhost:3247/todos', {
+    method: 'post',
+    body: new FormData(e.target)
   })
+  .then(response => response.json())
+  .then(render)
+})
