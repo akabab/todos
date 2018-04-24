ee0eca46cd45b7892a4e2c822cc970dc705914a4
Yoann Cribier <yoann_cribier@hotmail.com>
2018-04-24 21:25:26 +0200

    server - added multer to handle multipart form data, handles an image upload on /todos POST + bindings

--- server/index.js
+++ server/index.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const db = require('./db')
+const multer = require('multer')
+const fs = require('fs')
+const util = require('util')
+const path = require('path')
+
+const rename = util.promisify(fs.rename)
+
 const port = 3247
 
 const app = express()
+
+
+const publicImagesPath = path.join(__dirname, 'public/images')
+
+const storage = multer.diskStorage({
+  destination: publicImagesPath,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 500000, // 500 KB
+  }
+})
+
+const loggerMiddleware = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+}
+
+app.use(loggerMiddleware)
+
+app.use('/images', express.static(publicImagesPath))
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -10,45 +42,29 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use((req, res, next) => {
-  if (req.method === 'GET') return next()
-
-  let accumulator = ''
-
-  req.on('data', data => {
-    accumulator += data
-  })
-
-  req.on('end', () => {
-    try {
-      req.body = JSON.parse(accumulator)
-      next()
-    } catch (err) {
-      next(err)
-    }
-  })
-})
-
 app.get('/', (req, res) => {
   res.send('ok')
 })
 
-app.get('/todos', (req, res, next) => db.todo.read()
-  .then(todos => res.json(todos))
-  .catch(next))
+app.get('/todos', (req, res, next) => {
+  db.todo.read()
+    .then(todos => res.json(todos))
+    .catch(next)
+})
 
-app.post('/todos', (req, res, next) => {
+app.post('/todos', upload.single('image'), (req, res, next) => {
   db.todo.create({
-    userId: 2,
+    userId: 1, // TODO: use auth id
     title: req.body.title,
     description: req.body.description,
+    image: req.file ? req.file.filename : 'default.jpg'
   })
-  .then(() => res.json('ok'))
+  .then(() => db.todo.read().then(todos => res.json(todos)))
   .catch(next)
 })
 
 app.get('/todos/:id', async (req, res, next) => {
-  db.read.byId(req.params.id)
+  db.todo.read.byId(req.params.id)
     .then(todo => res.json(todo))
     .catch(next)
 })
