6ceb47cccb2ffce0ed9fb2a16570606aa38dd807
Yoann Cribier <yoann_cribier@hotmail.com>
2018-05-21 16:53:07 +0200

    Updates

--- js/components/todo.js
+++ js/components/todo.js
@@ -1,14 +1,39 @@
 import api from '../api.js'
 
 export const createTodoElement = todo => `
-  <a class='row todo fade' href='todo.html?id=${todo.id}'>
-    <div class='column column-10'>
-      <span>☆ ${todo.stars ? todo.stars.length : 0}</span>
+  <div class="container tile todo">
+    <div class="tile-icon hide-xs">
+      <div class="example-tile-icon">
+        <figure class="avatar avatar-lg">
+          <img src="${api.host}/images/${todo.image}" alt="Avatar">
+        </figure>
+      </div>
     </div>
-    <div class='column'>
-      <h4>${todo.title}</h4>
+    <div class="tile-content">
+      <p class="tile-title h5">${todo.title}</p>
+      <p class="tile-subtitle text-gray">${todo.description}</p>
+      <div class="divider"></div>
+      <div class="text-secondary text-italic text-right text-sm">by ${todo.author} - ${todo.createdAt.slice(0, 10)}</div>
     </div>
-  </a>
+    <div class="tile-action">
+      <button data-id=${todo.id} class="btn btn-sm btn-primary button-star">${document.user && todo.stars.includes(document.user.id) ? '★' : '☆'} ${todo.stars.length}</button>
+    </div>
+  </div>
+`
+
+export const noTodosContainer = `
+  <div id="todos" class="container">
+    <div class="empty">
+      <div class="empty-icon">
+        <i class="icon icon-people"></i>
+      </div>
+      <p class="empty-title h5">There is no todos yet</p>
+      <p class="empty-subtitle">Click the button to add a new todo.</p>
+      <div class="empty-action">
+        <button class="btn btn-primary">Add a todo</button>
+      </div>
+    </div>
+  </div>
 `
 
 export const createDetailedTodoElement = todo => `
--- js/index.js
+++ js/index.js
@@ -1,26 +1,48 @@
 import api from './api.js'
-import { createTodoElement } from './components/todo.js'
-
-const render = todos => {
-  const todosElement = document.getElementById('todos')
-
-  todosElement.innerHTML = todos.map(createTodoElement).join('') || '<div>No Todos</div>'
-}
-
-api.get('/todos').then(render)
+import { createTodoElement, noTodosContainer } from './components/todo.js'
 
 const formMessage = document.getElementById('add-todo-message')
-const form = document.getElementById('add-todo-form')
+
+const render = todos => {
+  // sort
+  todos.sort((a, b) => b.stars.length - a.stars.length)
+
+  const todosElement = document.getElementById('todos')
+
+  todosElement.innerHTML = todos.map(createTodoElement).join('') || noTodosContainer
+
+  const voteButtons = Array.from(document.getElementsByClassName('button-star'))
+  voteButtons.forEach(b => {
+    b.addEventListener('click', async e => {
+      e.preventDefault()
+
+      const todoId = e.target.dataset.id
+      api.get(`/todos/vote/${todoId}`)
+        .then(res => !res.error && api.get('/todos').then(render)) // refresh
+    })
+  })
+}
+
+const handleResponse = res => {
+  if (formMessage) {
+    formMessage.textContent = res.error || 'all good'
+  }
+
+  if (res.error) return
+
+  const todos = res
+  render(todos)
+}
+
+const form = document.getElementById('form-todos-add')
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  formMessage.textContent = ''
 
   const formData = new FormData(e.target)
 
-  api.post('/todos', formData)
-    .then(render)
-    .then(() => {
-      formMessage.innerHTML = 'all good'
-      setTimeout(() => formMessage.innerHTML = '', 1000)
-    })
+  api.post('/todos', formData).then(handleResponse)
 })
+
+api.get('/todos').then(render)
