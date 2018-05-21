093550cff663dcdfcee904d31e00f3290cfeb9c8
Yoann Cribier <yoann_cribier@hotmail.com>
2018-05-21 16:51:51 +0200

    implement vote button logic + sort todos on render based on stars count

--- client/js/index.js
+++ client/js/index.js
@@ -4,9 +4,23 @@ import { createTodoElement, noTodosContainer } from './components/todo.js'
 const formMessage = document.getElementById('add-todo-message')
 
 const render = todos => {
+  // sort
+  todos.sort((a, b) => b.stars.length - a.stars.length)
+
   const todosElement = document.getElementById('todos')
 
   todosElement.innerHTML = todos.map(createTodoElement).join('') || noTodosContainer
+
+  const voteButtons = Array.from(document.getElementsByClassName('button-star'))
+  voteButtons.forEach(b => {
+    b.addEventListener('click', async e => {
+      e.preventDefault()
+
+      const todoId = e.target.dataset.id
+      api.get(`/todos/vote/${todoId}`)
+        .then(res => !res.error && api.get('/todos').then(render)) // refresh
+    })
+  })
 }
 
 const handleResponse = res => {
